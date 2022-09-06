87f6e5cedf0397fb54ae06c5d766296095337ff6
Ottoniel Campos <hutzon4011@gmail.com>
2022-09-06 15:39:17 -0600

    subiendo tarea

---
+++ tarea.js
@@ -0,0 +1,11 @@
+const product = {
+    productName: 'Mi primera computadora',
+    price: 5000,
+    available: true
+}
+
+//se agregaron nuevas propiedades al objeto
+product.weight = '1.5Kg';
+product.measure = '1m';
+
+console.log(product);
